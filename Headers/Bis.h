b73c57e5f7230b7d35cb7d6ed07e553c58465fcc
FenFirasy <firasy720@gmail.com>
2025-11-29 22:10:33 +0700

    update isi Bis.h

--- Headers/Bis.h
+++ Headers/Bis.h
@@ -1,9 +1,10 @@
 #ifndef BIS_H_INLUDED
 #define BIS_H_INLUDED
-#include <iostream>
+#include "string"
 using namespace std;
 
 struct dataBis{
+    string idBis;
     int kapasitas;
     string rute;
 };
@@ -11,13 +12,16 @@ typedef struct elmBis *adrBis;
 struct elmBis{
     dataBis info;
     adrBis next;
+    adrPenumpang firstPenumpang;
 };
 
 struct ListBis{
     adrBis first;
 };
-void insertElmP(){}
-void deleteElmP(){}
-void findElmP(){}
-void displayP(){}
+void createListBis(ListBis &L);
+adrBis createElmBis(string idBis, int kapasitas, string rute);
+void insertElmBis(ListBis &L, adrBis P);
+void deleteElmBis(ListBis &L, adrBis &P);
+adrBis findBis(ListBis L, int idBis);
+void displayBis(ListBis L);
 #endif
