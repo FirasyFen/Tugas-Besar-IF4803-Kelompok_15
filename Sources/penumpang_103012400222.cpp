6c1bf02f65742c27c8e255997ae32adbe84946a6
Yafi <yafialifudinp@gmail.com>
2025-12-05 14:47:13 +0700

    update header penumpang dan cpp penumpang

---
+++ Sources/penumpang_103012400222.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "Penumpang.h"
+#include "Bis.h"
+using namespace std;
+adrPenumpang createElemenPenumpang(string tujuan, string idtiket){
+
+}
+
+void insertPenumpangFirst(ListPenumpang &p, adrPenumpang q){
+
+}
+
+void insertPenumpangAfter(ListPenumpang &p, adrPenumpang q){
+
+}
+
+void insertPenumpangLast(ListPenumpang &p, string  adrPenumpang q){
+
+}
+
+void deletePenumpang(ListPenumpang &p, string idtiket){
+
+}
+
+void ubahPenumpang(ListPenumpang &L, string idtiket, string tujuanBaru){
+
+}
+
+adrPenumpang searchPenumpang(ListPenumpang &p, string notiket, adrPenumpang idtiket){
+
+}
+
+void displayPenumpang(ListPenumpang p){
+
+}
+
+void deleteAllPenumpang(ListPenumpang &L){
+
+}
+
+int countPenumpang(ListPenumpang L){
+
+}
+
+bool isEmptyPenumpang(ListPenumpang L){
+
+}
+
+void displayPenumpangTujuan(ListPenumpang L, string tujuan){
+
+}
