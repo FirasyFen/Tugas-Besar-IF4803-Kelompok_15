459642e4b62580e78b592fb4a417a77e2beab784
FenFirasy <firasy720@gmail.com>
2025-12-06 18:01:37 +0700

    Update penumpang cpp

---
+++ Sources/penumpang_103012400366.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "Penumpang.h"
+#include "Bis.h"
+using namespace std;
+
+adrPenumpang createElemenPenumpang(string tujuan, string idtiket) {
+    adrPenumpang q = new elemenPenumpang;
+    q->info.tujuan = tujuan;
+    q->info.idtiket = idtiket;
+    q->next = nullptr;
+    q->prev = nullptr;
+    return q;
+}
+
+bool isEmptyPenumpang(adrPenumpang p) {
+    return (p == nullptr);
+}
+
+void insertPenumpangLast(adrPenumpang &p, adrPenumpang q) {
+    if (isEmptyPenumpang(p)) {
+        p = q;
+    } else {
+        adrPenumpang last = p;
+        while (last->next != nullptr) {
+            last = last->next;
+        }
+        last->next = q;
+        q->prev = last;
+    }
+}
+
+void deletePenumpangFirst(adrPenumpang &p, adrPenumpang &q) {
+    if (isEmptyPenumpang(p)) {
+        q = nullptr;
+        cout << "List penumpang kosong" << endl;
+    } else {
+        q = p;
+        p = q->next;
+        if (p != nullptr) {
+            p->prev = nullptr;
+        }
+        q->next = nullptr;
+        q->prev = nullptr;
+    }
+}
+
+void deletePenumpangAfter(adrPenumpang prec, adrPenumpang &q) {
+    if (prec == nullptr || prec->next == nullptr) {
+        q = nullptr;
+        cout << "Tidak ada elemen" << endl;
+        return;
+    }
+    q = prec->next;
+    prec->next = q->next;
+    if (q->next != nullptr) {
+        q->next->prev = prec;
+    }
+    q->next = nullptr;
+    q->prev = nullptr;
+}
+
+adrPenumpang searchPenumpang(adrPenumpang p, string idtiket) {
+    adrPenumpang q = p;
+    while (q != nullptr) {
+        if (q->info.idtiket == idtiket) {
+            return q;
+        }
+        q = q->next;
+    }
+    return nullptr;
+}
