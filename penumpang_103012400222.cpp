ad57620ba23d469fb9b9e3ff71e46c5f929b2e1a
Yafi <yafialifudinp@gmail.com>
2025-12-06 23:45:07 +0700

    update penumpang cpp

--- penumpang_103012400222.cpp
+++ penumpang_103012400222.cpp
@@ -3,11 +3,20 @@
 #include "Bis.h"
 using namespace std;
 void insertPenumpangFirst(adrBis &L, adrPenumpang p){
-    
+    if (isEmptyPenumpang(L)) {
+        L->firstPenumpang = p;
+        p->next = nullptr;
+        p->prev = nullptr;
+    } else {
+        p->next = L->firstPenumpang;
+        p->prev = nullptr;
+        L->firstPenumpang->prev = p;
+        L->firstPenumpang = p;
+    }
 }
 
 void insertPenumpangAfter(adrBis &L, adrPenumpang prec, adrPenumpang p){
-
+    
 }
 
 void deletePenumpangLast(adrBis &L, adrPenumpang &p){
@@ -15,11 +24,25 @@ void deletePenumpangLast(adrBis &L, adrPenumpang &p){
 }
 
 void ubahPenumpang(adrPenumpang &L, string idtiket, string tujuanBaru){
+     adrPenumpang p = L;
 
+    while (p != nullptr) {
+        if (p->info.idtiket == idtiket) {
+            p->info.tujuan = tujuanBaru;
+            return;
+        }
+        p = p->next;
+    }
 }
 
 void displayPenumpang(adrPenumpang L){
+    adrPenumpang p = L;
 
+    while (p != nullptr) {
+        cout << "ID Tiket: " << p->info.idtiket << endl;
+        cout << " Tujuan " << p->info.tujuan << endl;
+        p = p->next;
+    }
 }
 
 bool isEmptyPenumpang(adrBis L){
@@ -27,5 +50,12 @@ bool isEmptyPenumpang(adrBis L){
 }
 
 void displayPenumpangTujuan(adrPenumpang L, string tujuan){
+    adrPenumpang p = L;
 
+    while (p != nullptr) {
+        if (p->info.tujuan == tujuan) {
+            cout << p->info.idtiket << " - " << p->info.tujuan << endl;
+        }
+        p = p->next;
+    }
 }
