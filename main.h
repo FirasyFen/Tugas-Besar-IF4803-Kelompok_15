031fa68c9670acd40f625b3dfdf5d9fb507ac226
FenFirasy <firasy720@gmail.com>
2025-12-08 16:47:26 +0700

    update main header

--- main.h
+++ main.h
@@ -2,8 +2,71 @@
 #define MAIN_H_INLUDED
 using namespace std;
 
-typedef int infotype;
-struct main{
-    
+struct dataBis{
+    string idBis;
+    int kapasitas;
+    string rute;
 };
-#endif
+typedef struct elmBis *adrBis;
+typedef struct elemenPenumpang *adrPenumpang;
+struct elmBis{
+    dataBis info;
+    adrBis next;
+    adrPenumpang firstPenumpang;
+};
+struct ListBis{
+    adrBis first;
+};
+struct penumpang{
+    string idtiket;
+    string nama;
+    string tujuan
+};
+
+struct elemenPenumpang{
+    penumpang info;
+    adrPenumpang next;
+    adrPenumpang prev;
+};
+
+void createListBis(ListBis &L);
+adrBis createElmBis(string idBis, int kapasitas, string rute);
+bool isEmpty(ListBis L);
+void insertFirstBis(ListBis &L, adrBis P);
+void insertLastBis(ListBis &L, adrBis P);
+void insertAfterBis(ListBis &L, adrBis Prec, adrBis P);
+void deleteFirstBis(ListBis &L, adrBis &P);
+void deleteLastBis(ListBis &L, adrBis &P);
+void deleteAfterBis(ListBis &L, adrBis Prec, adrBis &P);
+adrBis findBis(ListBis L, string idBis);
+int countBis(ListBis L);
+void displayBis(ListBis L);
+adrBis findBisByRute(ListBis L, string rute);
+void adminInsertFirstBis(ListBis &L);
+void adminInsertLastBis(ListBis &L);
+void adminInsertAfterBis(ListBis &L);
+void adminDeleteFirstBis(ListBis &L);
+void adminDeleteLastBis(ListBis &L);
+void adminDeleteAfterBis(ListBis &L);
+adrBis inputBisData();
+adrPenumpang createElemenPenumpang(string nama, string tujuan, string idtiket);
+void insertPenumpangFirst(adrBis &L, adrPenumpang p);
+void insertPenumpangAfter(adrBis &L, adrPenumpang prec, adrPenumpang p);
+void insertPenumpangLast(adrBis &L,  adrPenumpang p);
+void deletePenumpangFirst(adrBis &L, adrPenumpang &p);
+void deletePenumpangAfter(adrBis &L, adrPenumpang prec, adrPenumpang &p);
+void deletePenumpangLast(adrBis &L, adrPenumpang &p);
+void ubahPenumpang(adrPenumpang &L, string idtiket, string tujuanBaru);
+adrPenumpang searchPenumpang(adrPenumpang &p, string notiket, adrPenumpang idtiket);
+void displayPenumpang(adrPenumpang L);
+void deleteAllPenumpang(adrPenumpang &L);
+int countPenumpang(adrPenumpang L);
+bool isEmptyPenumpang(adrPenumpang L);
+void displayPenumpangTujuan(adrPenumpang L, string tujuan);
+void adminInsertFirstPenumpang(ListBis &L);
+void adminInsertLastPenumpang(ListBis &L);
+void adminInsertAfterPenumpang(ListBis &L);
+void adminDeleteFirstPenumpang(ListBis &L);
+void adminDeleteLastPenumpang(ListBis &L);
+void adminDeleteAfterPenumpang(ListBis &L);
+adrPenumpang inputDataPenumpang();
