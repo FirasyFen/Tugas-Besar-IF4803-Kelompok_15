c1802606147ff4a8b73101e3aeb2dd092fb7ebce
Yafi <yafialifudinp@gmail.com>
2025-12-12 00:32:32 +0700

    update menu user

--- bis_103012400222.cpp
+++ bis_103012400222.cpp
@@ -53,3 +53,56 @@ void displayBis(ListBis L){
 bool isEmpty(ListBis L){
     return L.first == nullptr;
 }
+
+adrBis findBisByRute(ListBis L, string rute) {
+    adrBis P = L.first;
+    while (P != nullptr) {
+        if (P->info.rute == rute) {
+            return P;
+        }
+        P = P->next;
+    }
+    return nullptr;
+}
+
+adrBis userFindBisByRute(ListBis L) {
+    string rute;
+    cout << "Masukkan rute: ";
+    cin >> rute;
+
+    adrBis P = findBisByRute(L, rute);
+
+    if (P != nullptr) {
+        cout << "Bis ditemukan!\n";
+        cout << "ID Bis     : " << P->info.idBis << endl;
+        cout << "Kapasitas  : " << P->info.kapasitas << endl;
+        cout << "Rute       : " << P->info.rute << endl;
+    } else {
+        cout << "Tidak ada Bis dengan rute '" << rute << "'\n";
+    }
+
+    return P;
+}
+
+adrBis userFindBisById(ListBis L) {
+    string idBis;
+    cout << "Masukkan ID Bis: ";
+    cin >> idBis;
+
+    adrBis P = findBisById(L, idBis);
+
+    if (P != nullptr) {
+        cout << "Bis ditemukan!\n";
+        cout << "------------------------\n";
+        cout << "ID Bis     : " << P->info.idBis << endl;
+        cout << "Kapasitas  : " << P->info.kapasitas << endl;
+        cout << "Rute       : " << P->info.rute << endl;
+        cout << "------------------------\n";
+    } else {
+        cout << "Tidak ada Bis dengan ID '" << idBis << "'\n";
+    }
+
+    return P;
+}
+
+
